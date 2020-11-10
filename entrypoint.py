100e858472ff80ba5b533bfb207d9c8e19d84eef
Jerry <shijierui@gmail.com>
2020-11-10 22:16:11 +0000

    Standardized the module entering process in entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,29 +3,47 @@
 import diaryWritter
 import webBrowser.emailBrowser
 import myPlans.TODOs
+import finance.overhead
 import config
 
-while True:
-    choice = input('What would you like to do?\n---web (shortcut: email)\n---diary\n---todo\n')
-    if 'diary' in choice:
-        print(20 * '-' + 'enter diary section' + 20 * '-')
-        diaryWritter.main()
-        print (20*'-' + 'exit diary section' + 20*'-')
+dailyManagerModules = {
+    'diary': (diaryWritter.main, None),
+    'todo' : (myPlans.TODOs.main,(config.ACTIVE_FILEPATH, config.ARCHIEVE_FILEPATH)),
+    'overhead': (finance.overhead.main,(config.OVERHEAD_FILEPATH,))
+}
+#the dict above uses module name: (function, parameters) to record the information for each function,
+#use None if the funcion doesn't require a parameter and string 'userinput' to pass the command as a parameter
 
-    if choice[:5] == 'email':
-        print(20 * '-' + 'enter email section' + 20 * '-')
-        webBrowser.emailBrowser.main(choice)
-        print(20 * '-' + 'exit email section' + 20 * '-')
+def main():
+    inputPrompt = 'What would you like to do?' #entrypoint prompt for input
+    for module in dailyManagerModules:
+        inputPrompt += '\n---' + module # include the modules for the entrypoint
+    inputPrompt += '\nYour input:'
+    while True:
+        choice = input(inputPrompt)
+        for module in dailyManagerModules:
+            if module in choice:
+                print(20 * '-' + 'entering ' + module + ' section' + 20 * '-')
 
-    if 'web' in choice:
-        print(20 * '-' + 'enter web section' + 20 * '-')
-        webBrowser.emailBrowser.main()
-        print(20 * '-' + 'exit web section' + 20 * '-')
+                func, para = dailyManagerModules[module][0], dailyManagerModules[module][1]
+                if para:
+                    if para == 'userinput':
+                        # the case where the parameter is the userinput
+                        func(choice)
+                    else:
+                        # the case where there are parameters passed into the function
+                        func(*para)
+                else:
+                    # the case where the parameter is None
+                    func()
 
-    if 'todo' in choice:
-        print(20 * '-' + 'enter todo section' + 20 * '-')
-        myPlans.TODOs.main(config.ACTIVE_FILEPATH, config.ARCHIEVE_FILEPATH)
-        print(20 * '-' + 'exit todo section' + 20 * '-')
+                print (20*'-' + 'exiting ' + module + ' section' + 20*'-')
+                break #stop looking for other modules (only one modules can be used with one command in entry point)
+        else:
+            print('command is not understood.')
 
-    if input('terminate(y/n): ') == 'y':
-        break
+        if input('terminate(y/n): ') == 'y':
+            break #ask whether to end the whole loop after each command
+
+if __name__ == '__main__':
+    main()
